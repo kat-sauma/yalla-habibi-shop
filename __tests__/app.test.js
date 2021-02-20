c390c7d6f20f5204576a23a3487de11de623267c
Kat Sauma <katsauma@gmail.com>
2021-02-19 17:31:36 -0800

    second test running and second end point created for id of one item

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,53 +9,109 @@ const client = require('../lib/client');
 describe('app routes', () => {
   describe('routes', () => {
     let token;
-  
+
     beforeAll(async done => {
       execSync('npm run setup-db');
-  
+
       client.connect();
-  
+
       const signInData = await fakeRequest(app)
         .post('/auth/signup')
         .send({
           email: 'jon@user.com',
           password: '1234'
         });
-      
+
       token = signInData.body.token; // eslint-disable-line
-  
+
       return done();
     });
-  
+
     afterAll(done => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('returns clothes', async () => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'coolfactor': 3,
-          'owner_id': 1
+          id: 1,
+          name: 'Floral Silk Top',
+          img_url: '../assets/tops/blue-silk-top.jpg',
+          description: 'blue silk top with a pan collar',
+          category: 'vintage-tops',
+          owner_id: 1,
+          size: 'Large',
+          price: 60
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'coolfactor': 4,
-          'owner_id': 1
+          id: 2,
+          name: 'Denim Vest',
+          img_url: '../assets/tops/denim-vest-velvet-collar.jpg',
+          description: 'denim vest with a black suede collar',
+          category: 'vintage-tops',
+          owner_id: 1,
+          size: 'medium',
+          price: 45
         },
         {
-          'id': 3,
-          'name': 'spot',
-          'coolfactor': 10,
-          'owner_id': 1
+          id: 3,
+          name: 'Burberry Ruffle Collared Tee',
+          img_url: '../assets/tops/burberry-black-top.jpg',
+          description: 'black cotton polo with a ruffled collar',
+          category: 'vintage-tops',
+          owner_id: 1,
+          size: 'small',
+          price: 60
+        },
+
+        {
+          id: 4,
+          name: 'Beige Suede Button Down',
+          img_url: '../assets/tops/suede-button-down-top.jpg',
+          description: 'beige, patterned, suede button down tee shirt with structure',
+          category: 'vintage-tops',
+          owner_id: 1,
+          size: 'small',
+          price: 30
+        },
+
+        {
+          id: 5,
+          name: 'Purple Stripe Tank',
+          img_url: '../assets/tops/purple-stripe-tank.jpg',
+          description: 'lavender and mint striped spaghetti strap tank with diamond point hem',
+          category: 'vintage-tops',
+          owner_id: 1,
+          size: 'medium',
+          price: 20
         }
       ];
 
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/clothes')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expectation);
+    });
+
+    test('returns an item of clothing with its matching id', async () => {
+
+      const expectation = {
+        id: 3,
+        name: 'Burberry Ruffle Collared Tee',
+        img_url: '../assets/tops/burberry-black-top.jpg',
+        description: 'black cotton polo with a ruffled collar',
+        category: 'vintage-tops',
+        size: 'small',
+        price: 60,
+        owner_id: 1
+      };
+
+      const data = await fakeRequest(app)
+        .get('/clothes/3')
+
         .expect('Content-Type', /json/)
         .expect(200);
 
@@ -63,3 +119,13 @@ describe('app routes', () => {
     });
   });
 });
+
+
+
+//   const nothing = await fakeRequest(app)
+//     .get('/clothes/100')
+//     .expect('Content-Type', /json/)
+//     .expect(200);
+
+//   expect(nothing.body).toEqual('');
+// });
